4eb7c0fb4b4c89ea423997e1f93bc082b0734cc1
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-03-21 00:44:41 +0100

    added some basic REST API

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 #Aplicación DOTT
 
+##Instalar NodeJS
+###Descarga
+[NodeJS](https://nodejs.org/en/)
+
+##Instalar MongoDB
+###Descarga
+[MongoDB](https://www.mongodb.org/downloads#production)
+[Tutorial](https://docs.mongodb.org/manual/tutorial/install-mongodb-on-windows/)
+
+
 ##Instalar las dependencias:
 ```
 npm install
@@ -12,3 +22,6 @@ node server
 ```
 localhost:3000
 ```
+##Tutoriales NodeJS-MongoDB-Mongoose-APIREST
+
+[En español](https://carlosazaustre.es/blog/como-crear-una-api-rest-usando-node-js/)
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
 
+mongoose.connect('mongodb://localhost/dott');
+
+var User = require('./model/user');
+var Activity = require('./model/activity');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -10,8 +15,66 @@ var port = process.env.PORT || 3000;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'Hola!' });
+
+//Activities
+router.get('/activities', function(req, res) {
+  Activity.find(function(err, activities){
+    if(!err){
+      res.json(activities);
+    }else{
+      res.send(500, err.message);
+    }
+  });
+}).post('/activities', function(req, res) {
+  var activity = new Activity({
+    //set activity fields
+  });
+  activity.save(function(err, activity){
+    if(!err){
+      res.json(activity);
+    }else{
+      res.send(500, err.message);
+    }
+  });
+}).get('/activities/:activity_id', function(req, res) {
+  Activity.findById(req.params.id, function(err, activity){
+    if(!err){
+      res.json(activity);
+    }else{
+      res.send(500, err.message);
+    }
+  });
+}).put('/activities/:activity_id', function(req, res) {
+  Activity.findById(req.params.id, function(err, activity){
+    if(!err){
+      activity.name = req.body.name;
+      //set activity fields
+
+      activity.save(function(err, activity){
+        if(!err){
+          res.json(activity);
+        }else{
+          res.send(500, err.message);
+        }
+      });
+    }else{
+      res.send(500, err.message);
+    }
+  });
+}).delete('/activities/:activity_id', function(req, res) {
+  Activity.findById(req.params.id, function(err, activity){
+    if(!err){
+      activity.remove(function(err){
+        if(!err){
+          res.status(200).send();
+        }else{
+          res.send(500, err.message);
+        }
+      });
+    }else{
+      res.send(500, err.message);
+    }
+  });
 });
 
 app.use('/api', router);
