9df6cacce6aa2324162717d706633c8a02427a01
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-03-20 20:59:00 +0100

    Initial commit

---
+++ models/activity.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ActivitySchema = new Schema({
+  name:  String,
+  description: String,
+  image:   String,
+  location: String,
+  creator: {
+            userID: String,
+            name: String,
+            image: String
+          },
+  executionDate:   Date,
+  creationDate: Date,
+  minParticipants: Number,
+  maxParticipants: Number,
+  closed: Boolean,
+  score:[{comment: String, star: Number, userID: String, name: String, image: String}],
+  participants: [{userID: String, name: String, image: String}],
+  chat:[{message: String, date: Date, name: String, image: String}],
+  categories: [{name:String}]
+});
+
+module.exports = mongoose.model('Activity', ActivitySchema);
---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  name:  String,
+  username: String,
+  password:   String,
+  email:   String,
+  birthday: Date,
+  location: String,
+  image: String,
+  notifications: [{message: String, readed: Boolean, date: Date, activityID: String, activityName: String}]
+});
+
+module.exports = mongoose.model('User', UserSchema);
