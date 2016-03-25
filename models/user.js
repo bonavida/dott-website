bcec8a2fccb09f8ae3aab34e50a0be70a68a9387
al227760 <irene@192.168.1.132>
2016-03-25 12:32:18 +0100

    sjhls

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
   name:  String,
   username: String,
-  password:   String,
-  email:   String,
+  password: String,
+  email: String,
   birthday: Date,
   location: String,
   image: String,
