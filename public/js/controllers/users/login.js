37b6cc82aed3e66ab47234ffe8c5c26c0c0159f7
al227760 <irene@150.128.173.164>
2016-04-01 18:12:37 +0200

    algo...

--- public/js/controllers/users/login.js
+++ public/js/controllers/users/login.js
@@ -3,6 +3,19 @@
  */
 
 
-angular.module('dottApp.controllers').controller('LoginUserController', function($scope, Activity){
-  console.log("login");
+angular.module('dottApp.controllers').controller('LoginUserController', function($scope, $http, User){
+//  console.log("login");
+	$scope.username;
+	$scope.pwd;
+//	$scope.loginUser = function(){
+//		console.log("USUARIO!!!", $scope.username, $scope.pwd);
+//	};
+	$http.get("http://localhost:3000/api/users").then(function(response){
+//		$scope.username=;
+//		$scope.pwd;
+		lista=[{username:"a", pdw:"1"}, {username:"b", pdw:"2"}, {username:"c", pdw:"3"}];
+		console.log(lista)
+		$scope.a = expect(lista).toEqual({usename:$scope.username, pwd.$scope.pwd})
+		console.log("si?",a)
+	});
 });
