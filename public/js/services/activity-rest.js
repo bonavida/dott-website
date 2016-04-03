69565eef61afa46bdb35491a2ef5b9989b3d515f
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-04-04 00:06:25 +0200

    ActivityService rewrited

--- public/js/services/activity-rest.js
+++ public/js/services/activity-rest.js
@@ -1,7 +1,50 @@
-angular.module('dottApp.services').factory('Activity',function($resource,$location){
-    return $resource($location.absUrl().split("#")[0] + 'api/activities/:id',{id:'@_id'},{
-        update: {
-          method: 'PUT'
-        }
-    });
+angular.module('dottApp.services').factory('ActivityService', function($http, $q, $location){
+
+  var base_url = $location.absUrl().split("#")[0];
+
+  function getAll(){
+    var request = $http.get(base_url + 'api/activities');
+    return( request.then( handleSuccess, handleError ) );
+  }
+
+  function getByID(id){
+    var request = $http.get(base_url + 'api/activities/'+id);
+    return( request.then( handleSuccess, handleError ) );
+  }
+
+  function add(activity){
+    var request = $http.post(base_url + 'api/activities', activity);
+    return( request.then( handleSuccess, handleError ) );
+  }
+
+  function update(activity){
+    var request = $http.put(base_url + 'api/activities/'+activity.id, activity);
+    return( request.then( handleSuccess, handleError ) );
+  }
+
+  function remove(){
+    var request = $http.delete(base_url + 'api/activities/'+id);
+    return( request.then( handleSuccess, handleError ) );
+  }
+  //PRIVATE METHODS
+  function handleError( response ) {
+    if (
+        ! angular.isObject( response.data ) ||
+        ! response.data.message
+        ) {
+        return( $q.reject( "An unknown error occurred." ) );
+    }
+    return( $q.reject( response.data.message ) );
+  }
+  function handleSuccess( response ) {
+    return( response.data );
+  }
+
+  return ({
+    getAll    : getAll,
+    getByID   : getByID,
+    add       : add,
+    update    : update,
+    remove    : remove
+  });
 });
