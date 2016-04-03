3d69b50f83bad62a51e5584f1fa1f4755a40edf3
al227760 <irene@192.168.1.132>
2016-04-03 19:24:40 +0200

    Mensaje error en el Login

--- public/js/controllers/users/login.js
+++ public/js/controllers/users/login.js
@@ -2,25 +2,36 @@
  * http://usejsdoc.org/
  */
 
-
-angular.module('dottApp.controllers').controller('LoginUserController', function($scope, $http, User){
+angular.module('dottApp.controllers').controller('LoginUserController', function($scope, $http, $window, User){
 	$scope.username;
 	$scope.pwd;
 	$scope.list=[];
-	$scope.aux;	
+	$scope.aux;	//0-->NoLogin 1-->SiLogin 2-->CamposVacios
+//	$scope.alet;
 	
 	$http.get("http://localhost:3000/api/users").then(function(response){
-		$scope.list = response.data.records;
+//		$scope.list = response.data.records; //IMPORTANTE!NO ELIMINAR!
+		$scope.list = [{username:"a", pdw:"1"}, {username:"b", pdw:"2"}, {username:"c", pdw:"3"}];	//TODO QUITAR
 	});
 	
+	
 	$scope.login = function(){
-		$scope.aux=false;
+		$scope.aux=0;
+//		$scope.alet=""
 		angular.forEach($scope.list, function(value, key){
 			if(value.username==$scope.username && value.pdw==$scope.pwd){
 				console.log(true)
-				$scope.aux = true;
+				$scope.aux = 1;
+				$window.location.href="http://localhost:3000/#/activities";
 			}
 		});
+		
+		//No se han rellenado todavia lo campos, no quiero que se muestre ningun ERR
+		if(angular.isUndefined($scope.username) || angular.isUndefined($scope.pwd)){
+			console.log("undefined");
+			$scope.aux = 2;
+		}
+		
 	};
 	
 	$scope.login();
