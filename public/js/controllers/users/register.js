b7c0ce1f07a9f1843bcd43c852de4fdee8e616b3
Diego <al095660@uji.es>
2016-04-03 20:14:45 +0200

    Angular 1 - Diego 0

--- public/js/controllers/users/register.js
+++ public/js/controllers/users/register.js
@@ -1,7 +1,7 @@
 angular.module('dottApp.controllers').controller('UserRegisterController', function($scope) {
-  $scope.submitForm = function(isValid) {
-      if (isValid) {
-          alert('Our form is amazing!');
-      }
+    $scope.submitForm = function(isValid) {
+        if (isValid) {
+            alert('Our form is amazing!');
+        }
     };
 });
