dea0d8e268fbeb284ab11f13a9eba334ef17bddd
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-04-08 17:28:24 +0200

    fixed missed import

--- public/js/controllers/users/register.js
+++ public/js/controllers/users/register.js
@@ -1,9 +1,15 @@
-angular.module('dottApp.controllers').controller('UserRegisterController', function($scope) {
+angular.module('dottApp.controllers').controller('UserRegisterController', function($scope, $state, AuthService) {
     $scope.user = {};
 
     $scope.submitForm = function(isValid) {
         if (isValid) {
             console.log($scope.user);
+            AuthService.register($scope.user).then(function(){
+      				$state.go('activities');
+      				console.log("REDIRIGIR!");
+      			}).catch(function(){
+      				console.log("ERROR!");
+      			});
         }
     };
 });
