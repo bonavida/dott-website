0839e35807919ab01647eef5e4b88ad05e107712
al227760 <irene@192.168.1.132>
2016-04-12 23:41:04 +0200

    Imagen de perfil y formulario

--- public/js/controllers/users/register.js
+++ public/js/controllers/users/register.js
@@ -1,8 +1,9 @@
-angular.module('dottApp.controllers').controller('UserRegisterController', function($scope, $state, $timeout, AuthService) {
+angular.module('dottApp.controllers').controller('UserRegisterController', function($scope, $state, $timeout, AuthService, Upload) {
     $scope.user = {};
     $scope.user.birthday = new Date();
     $scope.msg = "";
     $scope.status = 2;
+    $scope.user.image = "images/profile.png";
 
     $scope.submitForm = function(isValid) {
         if (isValid) {
@@ -23,5 +24,32 @@ angular.module('dottApp.controllers').controller('UserRegisterController', funct
         $state.go('activities');
         console.log("REDIRIGIR!");
     }
+    
+    $scope.file="";
+    $scope.submit = function(){ //function to call on form submit
+        if ($scope.regForm.file.$valid && $scope.file) { //check if from is valid
+            $scope.upload($scope.file); //call upload function
+        }
+    };
 
+    $scope.upload = function (file) {
+        Upload.upload({
+            url: 'http://localhost:3000/api/uploads', //webAPI exposed to upload the file
+            data:{file:file} //pass file as data, should be user ng-model
+        }).then(function (resp) { //upload function returns a promise
+          console.log("....",resp);
+            if(resp.data.success){ //validate success
+                console.log('Success');
+                $scope.user.image = resp.data.url;
+            } else {
+                console.log('Error');
+            }
+        }, function (resp) { //catch error
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            console.log(evt);
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            $scope.progress = progressPercentage + '% subido';
+        });
+    };
 });
