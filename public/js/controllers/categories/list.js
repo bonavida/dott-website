b5fd63b4fe3a156f2a5bba37a9154821eeb86c1a
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-04-26 23:37:21 +0200

    Categories functional version

--- public/js/controllers/categories/list.js
+++ public/js/controllers/categories/list.js
@@ -1,4 +1,7 @@
 angular.module('dottApp.controllers').controller('ListCategoryController', function($scope, $state, CategoryService){
-
+  $scope.categories = [];
+  CategoryService.getAll().then(function(data){
+    $scope.categories = data;
+  });
 
 });
