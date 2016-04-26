c2b56be41f2f2e8ce06fea211541803477969ff8
Vlad Filip <vlad.filip.dc@gmail.com>
2016-04-26 23:38:24 +0200

    Fixed getByID for one activity

--- public/js/controllers/activities/view.js
+++ public/js/controllers/activities/view.js
@@ -1,14 +1,14 @@
 angular.module('dottApp.controllers').controller('ViewActivityController',function($scope, $stateParams, ActivityService){
-    //$scope.activity = {};
+    $scope.activity = {};
 
-    /*$scope.getActivity = function() {
-        ActivityService.getByID("571a969d4c5af2d771f1ada8").then(function (activity) {
+    $scope.getActivity = function() {
+        ActivityService.getByID($stateParams.id).then(function (activity) {
             $scope.activity = activity;
             console.log(activity.name);
         });
     };
 
     $scope.getActivity();
-    */
-    $scope.activity = ActivityService.getByID($stateParams.id);
+
+    //$scope.activity = ActivityService.getByID({id: $stateParams.id});
 });
