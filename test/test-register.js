d4b220aac175b40b7c57086945399021a86763dc
Diego <al095660@uji.es>
2016-04-26 10:15:01 +0200

    User registration tests fixed

--- test/test-register.js
+++ test/test-register.js
@@ -1,45 +1,38 @@
+var assert = require('chai').assert;
+var http = require('http');
+var rest = require('restler');
 
-var supertest = require("supertest");
-var should = require("should");
-var server = supertest.agent("http://localhost:300");
-
-describe('Resgitration API REST tests', function() {
+describe('User registration API REST tests', function() {
 	var user = {
 		username : "johndoe",
 		password : "12345678",
+        name : "John Doe",
 		email : "john@doe.com",
-		birthday : "1920-01-01T00:00:00.000Z",
+		birthday : "1970-01-01T00:00:00.000Z",
 		location : "Nowhere",
 		image : "images/profile.png"
 	};
 
-	it('should return valid user registration', function(done){
-        server
-        .post("/api/register")
-        .send(user)
-        .expect(200)
-        .end(function(err,res) {
-            res.body.success.should.equal(true);
-            done();
-        });
+	var base = "http://127.0.0.1:3000/";
+
+	it('Registrarse correctamente', function(done) {
+		rest.post(base+"api/register", {data: user}).on("complete", function(data) {
+			assert(data.success);
+			done();
+		});
 	});
 
-	it('Registrarse con un nombre de usuario ya existente', function(done){
-		user.email = "john@john.com";
-		user.username = "johndoe";
-		rest.post(base+"api/register", {data: user}).on("complete", function(data){
+	it('Registrarse con un nombre de usuario ya existente', function(done) {
+		rest.post(base+"api/register", {data: user}).on("complete", function(data) {
 			assert(!data.success);
 			done();
 		});
 	});
 
-	it('Registrarse con un e-mail ya existente', function(done){
-		user.username = "john";
-		user.password = "john@doe.com";
-		rest.post(base+"api/register", {data: user}).on("complete", function(data){
+	it('Registrarse con un e-mail ya existente', function(done) {
+		rest.post(base+"api/register", {data: user}).on("complete", function(data) {
 			assert(!data.success);
 			done();
 		});
 	});
-
 });
