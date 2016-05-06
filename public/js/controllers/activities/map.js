8b302ee875028c73231547650cf248d4dfc1c3dd
Diego <al095660@uji.es>
2016-05-06 09:56:57 +0200

    Angular Google Maps dependencies added.

--- public/js/controllers/activities/map.js
+++ public/js/controllers/activities/map.js
@@ -1,55 +1,43 @@
 angular.module('dottApp.controllers').controller('MapController',function($scope) {
-    $scope.lat;
-    $scope.lng;
-    $scope.status;
-    var map, marker;
-
-    $scope.init = function() {
-        $scope.lat = 39.9863563;
-        $scope.lng = -0.051324600000043574;
-        $scope.status = 0;
-
-        map = new google.maps.Map(document.getElementById('map-canvas'), {
-            center: {
-                lat: $scope.lat,
-                lng: $scope.lng
-            },
-            zoom : 15
-        });
-
-        marker = new google.maps.Marker({
-            position: {
-                lat: $scope.lat,
-                lng: $scope.lng
-            },
-            map: map,
-            draggable: true
-        });
-    };
-
-    var searchBox = new google.maps.places.SearchBox(document.getElementById('mapsearch'));
-
-    google.maps.event.addListener(searchBox, 'places_changed', function() {
-        var places = searchBox.getPlaces();
-
-        var bounds = new google.maps.LatLngBounds();
-
-        console.log("(" + places[0].geometry.location.lat()+ ", " + places[0].geometry.location.lng() + ")");
-        latLng(places);
-
-        bounds.extend(places[0].geometry.location);
-        marker.setPosition(places[0].geometry.location);
-
-        map.fitBounds(bounds);
-        map.setZoom(15);
+    // $scope.lat = 0;
+    // $scope.lng = 0;
+    //
+    // $scope.map = new google.maps.Map(document.getElementById('map-canvas'), {
+    //     center: {
+    //         lat: 39.9863563,
+    //         lng: -0.051324600000043574
+    //     },
+    //     zoom : 15
+    // });
+    //
+    // $scope.marker = new google.maps.Marker({
+    //     position: {
+    //         lat: 39.9863563,
+    //         lng: -0.051324600000043574
+    //     },
+    //     map: $scope.map,
+    //     draggable: true
+    // });
+    //
+    // var searchBox = new google.maps.places.SearchBox(document.getElementById('mapsearch'));
+    //
+    // google.maps.event.addListener(searchBox, 'places_changed', function() {
+    //     var places = searchBox.getPlaces();
+    //     var bounds = new google.maps.LatLngBounds();
+    //     var place;
+    //
+    //     for (var i = 0; place=places[i]; i++) {
+    //         bounds.extend(place.geometry.location);
+    //         $scope.marker.setPosition(place.geometry.location);
+    //         console.log("(" + place.geometry.location.lat()+ ", " + place.geometry.location.lng() + ")");
+    //         $scope.lat = place.geometry.location.lat();
+    //         $scope.lng = place.geometry.location.lng();
+    //     }
+    //
+    //     $scope.map.fitBounds(bounds);
+    //     $scope.map.setZoom(15);
     });
 
-    function latLng(places) {
-        $scope.lat = places[0].geometry.location.lat();
-        $scope.lng = places[0].geometry.location.lng();
-        $scope.status = 1;
-    }
 
-    $scope.init();
 
 });
