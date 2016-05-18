4a9506c746606497cf70926bcd7d255035446642
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-05-18 23:11:14 +0200

    search finished on list. redesigned list

--- public/js/controllers/activities/list.js
+++ public/js/controllers/activities/list.js
@@ -1,8 +1,28 @@
 angular.module('dottApp.controllers').controller('ListActivityController', function($scope, $stateParams, ActivityService){
   $scope.activities = [];
 
+  $scope.query = "";
+
+
+  $scope.search = function (row) {//Busqueda con filtros se ha de añadir una caso por cada columna disponible para ordenar
+    return (
+        /* (
+           $scope.nodeFilters[row.type]
+         ) &&*/
+       (
+         angular.lowercase(row.name).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
+         angular.lowercase(row.description).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
+         angular.lowercase(row.location.name).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
+         angular.lowercase(row.creator.name).indexOf(angular.lowercase($scope.query) || '') !== -1
+       )
+     );
+   };
+
   ActivityService.getAll().then(function(data){
       $scope.activities = data;
   });
+
+
+
 
 });
--- public/js/filters.js
+++ public/js/filters.js
@@ -4,4 +4,15 @@ angular.module('dottApp.filters')
     return function (text) {
         return text ? text.replace(/\n/g, '<br />') : '';
     };
+})
+.filter('range', function() {
+  return function(input, total) {
+    total = parseInt(total);
+
+    for (var i=0; i<total; i++) {
+      input.push(i);
+    }
+
+    return input;
+  };
 });
