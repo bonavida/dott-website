c8fd8b1676bc4f83dbb5cb554cb3b504db6f9c1d
Luby1917 <Luby.URSS.2.0@gmail.com>
2016-05-26 15:37:28 +0200

    Search on click, fixed participants bar issue

--- public/js/controllers/activities/list.js
+++ public/js/controllers/activities/list.js
@@ -1,10 +1,20 @@
-angular.module('dottApp.controllers').controller('ListActivityController', function($scope, $stateParams, ActivityService){
+angular.module('dottApp.controllers').controller('ListActivityController', function($scope, $stateParams, uiGmapGoogleMapApi, ActivityService){
   $scope.activities = [];
 
   $scope.query = "";
 
+  uiGmapGoogleMapApi.then(function (maps) {
+          $scope.$evalAsync(function () {
+              $scope.showMap = true;
+          });
+      });
+  $scope.search = function () {
+    $scope.query = $scope.searchText;
+  };
 
-  $scope.search = function (row) {//Busqueda con filtros se ha de añadir una caso por cada columna disponible para ordenar
+
+
+  $scope.searchFilter = function (row) {//Busqueda con filtros se ha de añadir una caso por cada columna disponible para ordenar
     return (
         /* (
            $scope.nodeFilters[row.type]
@@ -20,6 +30,7 @@ angular.module('dottApp.controllers').controller('ListActivityController', funct
 
   ActivityService.getAll().then(function(data){
       $scope.activities = data;
+      console.log(data);
   });
 
 
